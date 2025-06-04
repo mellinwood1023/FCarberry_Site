43a1395dd1c3c20d5a27d164764d0d5174a59e1b
Maggie Ellinwood <mellinwood1023@gmail.com>
2025-06-04 10:38:20 -0400

    installed updated tailwind - started form.tsx, adding files/components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to My Home Page</h1>
+      <p>This is your new homepage. Customize this content as you like!</p>
+    </div>
+  );
+};
+
+export default Home;
